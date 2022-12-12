12febff0de5285c942b579a1f779c5603f5559fd
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-12 16:42:40 +0000

    Adds User and UserBase classes

---
+++ phase_one/12_classes/userBase.js
@@ -0,0 +1,33 @@
+class UserBase {
+  constructor(users) {
+    this.users = users;
+  }
+
+  count() {
+    return this.users.length;
+  }
+
+  getNames() {
+    return this.users.map((user) => { return user.getName() });
+  }
+
+  getIntroductions() {
+    return this.users.map((user) => { return user.getIntroduction() });
+  }
+}
+
+module.exports = UserBase;
+
+// const User = require('./user');
+
+// const users = [
+//   new User('Uma'),
+//   new User('Josh'),
+//   new User('Ollie')
+// ];
+
+// const userBase = new UserBase(users);
+
+// console.log(userBase.count());
+// console.log(userBase.getNames());
+// console.log(userBase.getIntroductions());
