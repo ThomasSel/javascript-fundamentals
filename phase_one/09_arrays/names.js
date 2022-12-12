b79ebe46ea1774cea058da9872085a0376d5f362
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-12 15:22:00 +0000

    Adds array exercises

---
+++ phase_one/09_arrays/names.js
@@ -0,0 +1,8 @@
+const names = ['Shah', 'Robbie', 'Valerio', 'Chris', 'Marina', 'Anisha', 'Graeme'];
+
+console.log(names)
+console.log(names.length);
+console.log(names[1]);
+
+const newNames = names.concat('Thomas');
+console.log(newNames)
