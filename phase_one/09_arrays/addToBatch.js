0a7e5ca38e6b32896422cc5cca0b68fecafc6c9a
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-12 15:29:35 +0000

    Adds arrays challenge (addToBatch)

---
+++ phase_one/09_arrays/addToBatch.js
@@ -0,0 +1,10 @@
+const addToBatch = (batch, number) => {
+  if (batch.length >= 5) return batch;
+  return batch.concat(number);
+};
+
+console.log(addToBatch([1], 3)); 
+console.log(addToBatch([1, 2, 3], 4)); 
+console.log(addToBatch([], 8)); 
+console.log(addToBatch([1, 2, 3, 4, 5, 6], 7)); 
+console.log(addToBatch([1, 2, 3, 4, 5, 6, 7, 8, 9], 10)); 
