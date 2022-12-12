a81d238b441b4d58b7c0355467fe18fcb2d1a95e
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-12 16:21:44 +0000

    Updates messages.js with personalised discounts

--- phase_one/11_array_operations/messages.js
+++ phase_one/11_array_operations/messages.js
@@ -1,8 +1,14 @@
-const generateMessages = (names) => {
-  return names.map((name) => {
-    return `Hi ${name}! 50% off our best candies for you today!`;
+const generateMessages = (people) => {
+  return people.map((person) => {
+    return `Hi ${person.name}! ${person.discount}% off our best candies for you today!`;
   });
 };
-
-const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla'];
-console.log(generateMessages(names));
+// An array of objects!
+const namesAndDiscounts = [
+  { name: 'Anna', discount: 50 },
+  { name: 'Laura', discount: 40 },
+  { name: 'Josh', discount: 30 },
+  { name: 'Min', discount: 50 },
+  { name: 'Karla', discount: 60 }
+];
+console.log(generateMessages(namesAndDiscounts));
