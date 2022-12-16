f5a514ebda2f003798745baea34fd315822594c9
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-16 14:29:04 +0000

    Adds basic counter web app with express

---
+++ phase_two/simple-server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let counter = 0;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Thomas!');
+});
+
+app.post('/counter', (req, res) => {
+  counter++;
+  res.send();
+});
+
+app.get('/counter', (req, res) => {
+  res.send(`${counter}`);
+});
+
+app.delete('/counter', (req, res) => {
+  counter = 0;
+  res.send();
+});
+
+console.log(`Server listening on localhost:${port}`);
+app.listen(port);
